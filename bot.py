bfb5e9b14ecb9b7a40db51a1f777c56146cd4d5e
Liu Zhengyang <zhengyangliu@Lius-MacBook-Air-5.local>
2024-12-05 22:01:07 +0100

    Try to bugfix connection

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ intents.message_content = True       # Allows the bot to read message content
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 connection = get_connection()
+if connection is None:
+    print("fuck!")
 
 @bot.event
 async def on_ready():
