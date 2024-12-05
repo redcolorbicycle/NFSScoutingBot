fcef593eebecd9dc7ace6418f032ad50e451ab24
Liu Zhengyang <zhengyangliu@Lius-MacBook-Air-5.local>
2024-12-05 21:51:31 +0100

    Try to bugfix dotenv

--- bot.py
+++ bot.py
@@ -2,10 +2,8 @@ import discord
 from discord.ext import commands
 from database import get_connection
 from commands import setup_commands
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 # Define intents
 intents = discord.Intents.default()  # Default intents include basic events
 intents.message_content = True       # Allows the bot to read message content
