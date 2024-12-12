101fdf8a382f63bec8045708abaae7eb8aa7a303
Zhengyang <liuzhengyang10@gmail.com>
2024-12-12 17:18:54 +0100

    remove buggy version of 5t

--- bot_commands/misc_commands.py
+++ bot_commands/misc_commands.py
@@ -80,34 +80,7 @@ class MiscCommands(commands.Cog):
                       )      
             await ctx.send(answer)  
     
-    @commands.command()
-    async def fivetoolcalculator(self, ctx, totalcon:int, totalpow:int, totaleye:int, totalspd:int,
-                                 totalfld:int, target:int, supreme:str):
-        """
-        Calculates how many points are needed to hit a threshold and how many points leftover
-        """
-        supreme = supreme.lower()
-        trainingtotal = 57
-        totalstats = totalcon + totalpow + totaleye + totalspd + totalfld
-        if supreme == "yes":
-            trainingtotal = 87
-        if target*5 - (totalstats + trainingtotal) > 0:
-            await ctx.send("You're fucked, that's out of reach.")
-        else:
-            contrain = max(target - totalcon,0)
-            powtrain = max(target - totalpow,0)
-            eyetrain = max(target - totaleye,0)
-            spdtrain = max(target - totalspd,0)
-            fldtrain = max(target - totalfld,0)
-
-            answer = (f"You need {contrain} points to Contact.\n"
-                      f"You need {powtrain} points to Power.\n"
-                      f"You need {eyetrain} points to Eye.\n"
-                      f"You need {spdtrain} points to Speed.\n"
-                      f"You need {fldtrain} points to Fielding.\n"
-                      f"You will have {trainingtotal - contrain - powtrain - eyetrain - spdtrain - fldtrain} training points left over.\n"
-                      )      
-            await ctx.send(answer)  
+    
 
 
 async def setup(bot):
