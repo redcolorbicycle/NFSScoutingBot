7229f98eecd64d3fd5768a2fa3f4c75fc77a432b
Zhengyang <liuzhengyang10@gmail.com>
2024-12-12 17:14:20 +0100

    override version 5tool

--- bot_commands/misc_commands.py
+++ bot_commands/misc_commands.py
@@ -65,17 +65,17 @@ class MiscCommands(commands.Cog):
         if target*5 - (totalstats + trainingtotal) > 0:
             await ctx.send("You're fucked, that's out of reach.")
         else:
-            contrain = target - totalcon
-            powtrain = target - totalpow
-            eyetrain = target - totaleye
-            spdtrain = target - totalspd
-            fldtrain = target - totalfld
+            contrain = max(target - totalcon,0)
+            powtrain = max(target - totalpow,0)
+            eyetrain = max(target - totaleye,0)
+            spdtrain = max(target - totalspd,0)
+            fldtrain = max(target - totalfld,0)
 
-            answer = (f"You need {max(contrain, 0)} points to Contact.\n"
-                      f"You need {max(powtrain, 0)} points to Power.\n"
-                      f"You need {max(eyetrain, 0)} points to Eye.\n"
-                      f"You need {max(spdtrain, 0)} points to Speed.\n"
-                      f"You need {max(fldtrain, 0)} points to Fielding.\n"
+            answer = (f"You need {contrain} points to Contact.\n"
+                      f"You need {powtrain} points to Power.\n"
+                      f"You need {eyetrain} points to Eye.\n"
+                      f"You need {spdtrain} points to Speed.\n"
+                      f"You need {fldtrain} points to Fielding.\n"
                       f"You will have {trainingtotal - contrain - powtrain - eyetrain - spdtrain - fldtrain} training points left over.\n"
                       )      
             await ctx.send(answer)  
@@ -94,17 +94,17 @@ class MiscCommands(commands.Cog):
         if target*5 - (totalstats + trainingtotal) > 0:
             await ctx.send("You're fucked, that's out of reach.")
         else:
-            contrain = target - totalcon
-            powtrain = target - totalpow
-            eyetrain = target - totaleye
-            spdtrain = target - totalspd
-            fldtrain = target - totalfld
+            contrain = max(target - totalcon,0)
+            powtrain = max(target - totalpow,0)
+            eyetrain = max(target - totaleye,0)
+            spdtrain = max(target - totalspd,0)
+            fldtrain = max(target - totalfld,0)
 
-            answer = (f"You need {max(contrain, 0)} points to Contact.\n"
-                      f"You need {max(powtrain, 0)} points to Power.\n"
-                      f"You need {max(eyetrain, 0)} points to Eye.\n"
-                      f"You need {max(spdtrain, 0)} points to Speed.\n"
-                      f"You need {max(fldtrain, 0)} points to Fielding.\n"
+            answer = (f"You need {contrain} points to Contact.\n"
+                      f"You need {powtrain} points to Power.\n"
+                      f"You need {eyetrain} points to Eye.\n"
+                      f"You need {spdtrain} points to Speed.\n"
+                      f"You need {fldtrain} points to Fielding.\n"
                       f"You will have {trainingtotal - contrain - powtrain - eyetrain - spdtrain - fldtrain} training points left over.\n"
                       )      
             await ctx.send(answer)  
