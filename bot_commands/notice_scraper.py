2240bc1ad45b45dce0db7c9ab842d3d080bed459
Zhengyang <liuzhengyang10@gmail.com>
2024-12-18 02:05:16 +0100

    fuck this

--- bot_commands/notice_scraper.py
+++ bot_commands/notice_scraper.py
@@ -36,8 +36,7 @@ class NoticeScraper(commands.Cog):
 
         try:
             response = requests.post(self.api_url, headers=headers, json=payload)
-            print(f"Response Status: {response.status_code}")
-            print("Response Content:", response.text[:2000])  # Debug response content
+            print("Response Content:", response.text[:9000])  # Debug response content
 
             if response.status_code == 200:
                 try:
